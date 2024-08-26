34b980403763134a644929322b3eecc17017d07a
Andre <andretrevizam264@gmail.com>
2024-08-26 18:52:21 -0300

    operadores

---
+++ tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args) {
+//        String nome = "André " + "Trevizam";
+//        System.out.println(nome);
+//
+//        String concatenacao = "?";
+//        concatenacao = 1 + 1 + 1 + "1";
+//        System.out.println(concatenacao);
+
+//        int num = 5;
+//        num = -num; // transforma o num em negativo
+//        System.out.println(num);
+//
+//        num = num * -1; // para fazer ele voltar a ser positivo multiplicamos por -1
+//        System.out.println(num);
+
+        int num = 5;
+        num ++;
+
+        System.out.println(num);
+
+        boolean var = false;
+        var = !var;
+
+        System.out.println(var);
+    }
+}
--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // short numCurto = 1;
+        // int numNormal = numCurto;
+        // short numCurto2 = numNormal;
+
+        final String MEU_NOME = "André";
+
+        final double VALOR_PI = 3.14; // constantes sempre escritas em CAPS LOCK
+        System.out.println("Meu nome é " + MEU_NOME + ". E o valor de PI é: " + VALOR_PI);
     }
 }
